954c700b821fff22316a01b73cf88132d50fd614
matej ferenc <matej.ferenc@gmail.com>
2015-07-16 22:32:58 +0200

    photos

---
+++ js/nav.js
@@ -0,0 +1,3 @@
+$(function() {
+	$("nav").prepend("<section class='top-bar-section'><ul><li><a href='photos.html'>Photos</a></li></ul><ul><li><a href='#'>Measurements</a></li></ul><ul><li><a href='#'>Contact</a></li></ul></section>");
+})
