7de2a64cd2f383d7ec08ff6671270ef8ba816252
Matej Ferenc <Matej.Ferenc@4finance.com>
2015-07-18 14:25:24 +0200

    Color and home

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,3 @@
 $(function() {
-	$("nav").prepend("<section class='top-bar-section'><ul><li><a href='photos.html'>Photos</a></li></ul><ul><li><a href='measurements.html'>Measurements</a></li></ul><ul><li><a href='contact.html'>Contact</a></li></ul></section>");
+	$("nav").prepend("<section class='top-bar-section'><ul><li><a href='index.html'>Home</a></li></ul><section class='top-bar-section'><ul><li><a href='photos.html'>Photos</a></li></ul><ul><li><a href='measurements.html'>Measurements</a></li></ul><ul><li><a href='contact.html'>Contact</a></li></ul></section>");
 })
